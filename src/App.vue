1403c3c491e329ccc75a28d60f1e95507b246ae5
samer <judosamer555@gmail.com>
2025-09-03 20:36:03 +0300

    last commit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
-// import { defineAsyncComponent } from "vue";
-// import ReactInVue from "./utils/ReactInVue";
+import { defineAsyncComponent } from "vue";
+import ReactInVue from "./utils/ReactInVue";
 
-// const RemoteReactApp = defineAsyncComponent(() => import("remote_app/App").then((module) => ReactInVue(module.default)));
+const RemoteReactApp = defineAsyncComponent(() => import("remote_app/App").then((module) => ReactInVue(module.default)));
 </script>
 
 <template>
   <div>
     <h1>Vue Host Application</h1>
-    <!-- <RemoteReactApp /> -->
+    <RemoteReactApp />
   </div>
 </template>
